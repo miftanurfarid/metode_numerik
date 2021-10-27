7f80ce736211c9d8a653aa2c96ad8ae8eb473b07
miftanurfarid <miftanurfarid@gmail.com>
2021-10-27 08:57:53 +0800

    update slide dan source code

---
+++ slide/2021/03.solusi_persamaan_lanjar/code/03.eliminasi_gauss_modified.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 26 16:42:07 2021
+
+@author: fafa
+"""
+
---
+++ slide/2021/03.solusi_persamaan_lanjar/code/05.metode_iterasi_jacobi.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 26 16:44:08 2021
+
+@author: fafa
+"""
+
+import numpy as np
+
+a = np.array([[3, 1, -1],[2, 4, 1],[-1, 5, 8]])
+b = np.array([[1],[5],[8]])
+
