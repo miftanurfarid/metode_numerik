11ee4901b873243716d844e26796fa45e524de29
miftanurfarid <miftanurfarid@gmail.com>
2023-09-18 14:17:21 +0800

    code

---
+++ code/2023/kuis1/metode_numerik.py
@@ -0,0 +1,19 @@
+def iterasi_biasa():
+    return None
+
+
+def newton_raphson():
+    return None
+
+
+def bagi_dua():
+    return None
+
+
+def regula_falsi():
+    return None
+
+
+def secant():
+    return None
+
