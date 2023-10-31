96ca58b7cc4f2f9fd2655f9bf0ccbd52727e57d9
miftanurfarid <miftanurfarid@gmail.com>
2023-10-31 09:02:00 +0800

    interpolasi

--- code/2023/02.interpolasi_regresi/interpolasi_lagrange.py
+++ code/2023/02.interpolasi_regresi/interpolasi_lagrange.py
@@ -10,7 +10,7 @@ x = [-1.2, 0.3, 1.1]
 y = [-5.76, -5.61, -3.69]
 m = len(x)
 n = m-1
-xp = float(input('Masukkan nilai x: '))
+xp = 0 #float(input('Masukkan nilai x: '))
 
 yp = 0
 for i in range(n+1):
@@ -19,4 +19,4 @@ for i in range(n+1):
         if j != i:
             L *= (xp - x[j])/(x[i] - x[j])
     yp += y[i]*L
-print('Untuk x = %.1f, y = %.4f' % (xp, yp))
+print('Untuk x = %.4f, y = %.4f' % (xp, yp))
